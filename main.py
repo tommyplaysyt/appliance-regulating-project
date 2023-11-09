7fc1ab3a586718c84ce092a22c3677ade3785db0
tommyplaysyt <91047065+tommyplaysyt@users.noreply.github.com>
2023-11-09 12:17:50 +1000

    Add comments & light sensor components

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Temperature Sensor
+
 let reading = 0
 let temp_c = 0
 basic.forever(function () {
@@ -5,4 +7,13 @@ basic.forever(function () {
     temp_c = Math.idiv(reading * 75, 1000) - 14
     basic.showNumber(temp_c)
     basic.pause(500)
+})
+
+# Light Sensor
+
+basic.forever(function () {
+    led.plotBarGraph(
+    pins.analogReadPin(AnalogPin.P2),
+    100
+    )
 })
